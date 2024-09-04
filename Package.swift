5a03d943857f21d03b18cca1266b6c096b72db23
Iván Cáceres <ztmobile@zytrust.com>
2024-09-04 15:55:00 -0500

    6.4.6

--- Package.swift
+++ Package.swift
@@ -14,24 +14,16 @@ let package = Package(
         )
     ],
     dependencies: [
-        // Dependencias de otros paquetes
         .package(url: "https://github.com/ZYTRUST/BiometricSDK.git", from: "4.34.10"),
         .package(url: "https://github.com/ZYTRUST/BiometricSDKAlgorithmPlugin_F6_0_IDD80.git", from: "4.34.4"),
         .package(url: "https://github.com/ZYTRUST/BiometricSDKFaceCapturePluginNormal.git", from: "4.34.4"),
         .package(url: "https://github.com/ZYTRUST/BiometricSDKUIFaceModePassiveCore.git", from: "2.3.5"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.1")
-
     ],
     targets: [
         .binaryTarget(
             name: "zy_lib_idemia_face_ios",
             path: "zy_lib_idemia_face_ios.xcframework"
-        ), 
-        .target(
-            name: "zy_lib_idemia_face_ios_resources",
-            resources: [
-                .process("zy_lib_idemia_face_ios.bundle")
-            ]
         )
     ]
 )
